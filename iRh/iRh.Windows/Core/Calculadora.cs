f649030d18c7588fc156863c4c7c5b4c866e73b7
Danillo Lopes <lopessousa2004@gmail.com>
2023-05-23 21:07:41 -0300

    arquivo

--- iRh/iRh.Windows/Core/Calculadora.cs
+++ iRh/iRh.Windows/Core/Calculadora.cs
@@ -2,27 +2,49 @@
 {
     public static class Calculadora
     {
-        //FUNCAO   RETORNO  NOME ()
+        const double Faixa01 = 1302.0;
+        const double Faixa02 = 2571.29;
+        const double Faixa03 = 3856.94;
+        const double Faixa04 = 7507.49;
         public static double Inss(double salario)
         {
-            const double Faixa01 = 1302.0;
-            const double Faixa02 = 2571.29;
-            const double Faixa03 = 3856.94;
-            const double Faixa04 = 7507.49;
 
             double valorInss = 0;
 
             if(salario <= Faixa01)
             {
-                valorInss = (0.075 * salario);
+                valorInss = Calculadora.CalculaFaixa1(salario);
 
             }else if(salario <= Faixa02)
             {
-
+                valorInss = 0.09 * ((salario - Faixa01) + Calculadora.CalculaFaixa1(salario));
+            }else if(salario <= Faixa03)
+            {
+                valorInss = 0.12 * ((salario - Faixa02) + Calculadora.CalculaFaixa1(salario) + Calculadora.CalculaFaixa2(salario));
+            }else if(salario <= Faixa04)
+            {
+                valorInss = 0.14 * ((salario - Faixa03) + Calculadora.CalculaFaixa1(salario) + Calculadora.CalculaFaixa2(salario) + Calculadora.CalculaFaixa3(salario));
+            }else
+            {
+                valorInss = Calculadora.CalculaFaixa1(salario) + Calculadora.CalculaFaixa2(salario) + Calculadora.CalculaFaixa3(salario) + Calculadora.CalculaFaixa4(salario);
             }
-
-
-            return 0;
+            return valorInss;
+        }
+        private static double CalculaFaixa1(double salario)
+        {
+            return(0.075 * salario);
+        }
+        private static double CalculaFaixa2(double salario)
+        {
+            return(0.09 * (Faixa02 - Faixa01));
+        }
+        private static double CalculaFaixa3(double salario)
+        {
+            return(0.12 * (Faixa03 - Faixa02));
+        }
+        private static double CalculaFaixa4(double salario)
+        {
+            return(0.14 * (Faixa04 - Faixa03));
         }
     }
 }
